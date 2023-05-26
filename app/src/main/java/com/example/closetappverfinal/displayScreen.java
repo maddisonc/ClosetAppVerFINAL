c08c52a548c092e0f458e061f98d6f40e2ef646d
Maddison <mtranchau1@gmail.com>
2023-05-25 20:24:05 -0700

    Buttons functional.

--- app/src/main/java/com/example/closetappverfinal/displayScreen.java
+++ app/src/main/java/com/example/closetappverfinal/displayScreen.java
@@ -2,13 +2,51 @@ package com.example.closetappverfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class displayScreen extends AppCompatActivity {
+    private Button seeAllBtn;
+    private Button backToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_screen);
-    }
-}
+
+        // seeAll button
+        seeAllBtn = findViewById(R.id.seeAllBtn);
+        // backToMain button
+        backToMain = findViewById(R.id.backToMain);
+
+        seeAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                configureSeeAllBtnButton();
+            }
+        }); // to seeAllScreen method
+
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                configureBackMainBtnButton();
+            }
+        }); // to backToMain method
+
+    } // end onCreate
+
+    public void configureSeeAllBtnButton ()
+    {
+        Intent intent = new Intent(this, seeAllScreen.class);
+        startActivity(intent);
+    } // end seeAll button handler
+
+    public void configureBackMainBtnButton ()
+    {
+        Intent intent = new Intent(this, mainMenu.class);
+        startActivity(intent);
+    } // end backToMain button handler
+
+} // end display class
