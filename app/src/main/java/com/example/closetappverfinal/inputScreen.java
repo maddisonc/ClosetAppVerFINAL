5b7f18a6b5afd74f4f844f769173fba6565be52b
Maddison <mtranchau1@gmail.com>
2023-05-25 20:02:59 -0700

    Created String vars to hold user input, wrote getters to access attributes on next screen.

--- app/src/main/java/com/example/closetappverfinal/inputScreen.java
+++ app/src/main/java/com/example/closetappverfinal/inputScreen.java
@@ -6,9 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class inputScreen extends AppCompatActivity {
     private Button submitBtn;
+
+    // stores clothing attributes
+    public static String link;
+    public static String name;
+    public static String color;
+    public static String formality;
+    public static String weather;
+
+    // stores user's input
+    private EditText linkInput;
+    private EditText nameInput;
+    private EditText colorInput;
+    private EditText formalityInput;
+    private EditText weatherInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +38,36 @@ public class inputScreen extends AppCompatActivity {
                 configureSubmitButton();
             }
         }); // next activity method
+
+        linkInput = findViewById(R.id.picLinkInput);
+        nameInput = findViewById(R.id.nameInput);
+        colorInput = findViewById(R.id.colorInput);
+        formalityInput = findViewById(R.id.formalityInput);
+        weatherInput = findViewById(R.id.weatherInput);
+
     } // end onCreate
+
+    // getters
+    public static String getLink()
+    {
+        return link;
+    }
+    public static String getName()
+    {
+        return name;
+    }
+    public static String getColor()
+    {
+        return color;
+    }
+    public static String getFormality()
+    {
+        return formality;
+    }
+    public static String getWeather()
+    {
+        return weather;
+    }
 
     public void configureSubmitButton ()
     {
