19d6503dba1f51cd43ff8ffcea430aee52e5fda3
Maddison <mtranchau1@gmail.com>
2023-05-25 19:15:49 -0700

    Splash screen completely functional.

--- app/src/main/java/com/example/closetappverfinal/splashScreen.java
+++ app/src/main/java/com/example/closetappverfinal/splashScreen.java
@@ -2,7 +2,9 @@ package com.example.closetappverfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splashScreen extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-    }
-}
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(splashScreen.this, mainMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    } // end onCreate
+} // end splash class
