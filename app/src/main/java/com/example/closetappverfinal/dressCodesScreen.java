92a06ecdb2246680bffc11dbb830c52a9c2ef235
Maddison <mtranchau1@gmail.com>
2023-05-26 01:19:47 -0700

    Created screen and scrollview including descriptions for each dress code.  Next steps: add button to navigate to this screen on mani menu, add button going back to main menu on the dress codes screen.

--- app/src/main/java/com/example/closetappverfinal/dressCodesScreen.java
+++ app/src/main/java/com/example/closetappverfinal/dressCodesScreen.java
@@ -3,12 +3,34 @@ package com.example.closetappverfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class dressCodesScreen extends AppCompatActivity {
+
+    // https://emilypost.com/advice/attire-guide-dress-codes-from-casual-to-white-tie
+    String[] dressCodesList = {
+            "Casual: informal and comfortable (Plain T-shirts, shorts)\n",
+            "Semi-formal: office-wear (dress shirts, less formal dresses)\n",
+            "Business Formal: formal wear for business events (matching pairs of suit jackets and pants/skirts)\n",
+            "Business casual: slacks, khakis, dress shirts/blouses\n",
+            "Black tie: weddings, proms, and formal dinners (floor length evening gowns, black tuxedos)\n",
+            "White Tie: the most formal attire - implies that guests are socially distinguished (royals, celebrities)"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dress_codes_screen);
+        fillScrollView();
     }
-}
+
+    public void fillScrollView ()
+    {
+        String output = "";
+        TextView dressCodes = (TextView) findViewById(R.id.scrollViewText);
+        for (String a : dressCodesList)
+        {
+            output += "\n" + a;
+        }
+        dressCodes.setText(output);
+    } // end fill method
+} // end dressCodes class
