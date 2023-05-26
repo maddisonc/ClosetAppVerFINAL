26377fca8e16f87284f57cde1e64041194df149c
Maddison <mtranchau1@gmail.com>
2023-05-25 20:12:26 -0700

    Vars created to store filters and getters created to access inputs on the next screen.

--- app/src/main/java/com/example/closetappverfinal/filterScreen.java
+++ app/src/main/java/com/example/closetappverfinal/filterScreen.java
@@ -3,12 +3,44 @@ package com.example.closetappverfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class filterScreen extends AppCompatActivity {
+
+    private Button submitBtn;
+
+    // stores filters
+    public static String color;
+    public static String formality;
+    public static String weather;
+
+    private EditText colorInput;
+    private EditText formalityInput;
+    private EditText weatherInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_screen);
+
+        colorInput = findViewById(R.id.colorInput);
+        formalityInput = findViewById(R.id.formalityInput);
+        weatherInput = findViewById(R.id.weatherInput);
+    } // end onCreate
+
+    // getters
+    public static String getColorFilter()
+    {
+        return color;
     }
-}
+    public static String getFormalityFilter()
+    {
+        return formality;
+    }
+    public static String getWeatherFilter()
+    {
+        return weather;
+    }
+
+} // end filter class
